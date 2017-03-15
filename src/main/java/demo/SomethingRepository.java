11d1148c33db0fe8228cfbc7feaf70eb35170742
黄玉强 <huang.yu.qiang@163.com>
2017-03-15 18:05:06 +0800

    learn repository abstraction web support

--- src/main/java/demo/SomethingRepository.java
+++ src/main/java/demo/SomethingRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 /**
  * @author YQ.Huang
  */
-// NOTE: @Repository is not mandatory
+// NOTE: @Repository is not mandatory, but add it explicitly would enable DataAccessException conversion.
 @Repository
 public interface SomethingRepository extends MongoRepository<Something, String> {
 }
